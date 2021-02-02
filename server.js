0cdb62b10a64099b14e11397dcbb3a3451fc8e6f
Glitch (hello-express) <none>
2021-02-02 13:52:15 +0000

    🎫🎊 Checkpoint ./server.js:27141663/8355

--- server.js
+++ server.js
@@ -34,3 +34,62 @@ bot.variables({
   myName: "money",
   money: 500
 });
+
+bot.command({
+  name: "roulette",
+  code:`$onlyIf[$checkContains[$toLowercase[$message[2]];red;black]==true;Please pick a side, either **red** or **black**]
+
+$onlyIf[$message[1]<=$getUserVar[money];You don't have enough money! **$message[1]**]
+
+$argsCheck[>1; Please Put a price and a side, red or black! and If you don't understand it type `s.helproulette`]
+
+$authorIcon[$authorAvatar]
+$author[$username#$discriminator[$authorID]]
+$description[$username bet on $**$message[1]** and picked **`$message[2]`**]
+$footer[Hold on for 25s!]
+$color[ff354]`
+  })
+  
+bot.command({
+  name: "roulettle",
+  code: `$replyIn[25s]
+$suppressErrors
+
+$setUserVar[money;$$randomText[sum;sub][$getUserVar[money];$message[1]]]
+
+$title[$replaceText[$replaceText[$randomText[sum;sub];sum; WINNER;1];sub; Unfortunately $username lost.;1]]
+
+$description[ball $replaceText[$replaceText[$randomText[sum;sub];sum; draw;1];sub; draw;1] $replaceText[$replaceText[$randomText[sum;sub];sum;on: **`$message[2]`**;1];sub;on: **`$randomText[red;black]`**;1]
+
+$replaceText[$replaceText[$randomText[sum;sub];sum; <@$authorID> $username won: $$message[1];1];sub;<@$authorID> you lost: $$message[1];1]]
+$footer[$username#$discriminator[$authorID]]
+$footerIcon[$userAvatar[$authorID]]
+$color[$replaceText[$replaceText[$randomText[sum;sub];sum;1bbe00;1];sub;eb0000;1]]
+
+$onlyIf[$checkContains[$toLowercase[$message[2]];red;black]==true;]
+
+$onlyIf[$message[1]<=$getUserVar[money];]
+
+$argsCheck[>1;]`
+
+$replyIn[25s]
+$suppressErrors
+
+$setUserVar[money;$$randomText[sum;sub][$getUserVar[money];$message[1]]]
+
+$title[$replaceText[$replaceText[$randomText[sum;sub];sum; WINNER;1];sub; Unfortunately $username lost.;1]]
+
+$description[ball $replaceText[$replaceText[$randomText[sum;sub];sum; draw;1];sub; draw;1] $replaceText[$replaceText[$randomText[sum;sub];sum;on: **`$message[2]`**;1];sub;on: **`$randomText[red;black]`**;1]
+
+$replaceText[$replaceText[$randomText[sum;sub];sum; <@$authorID> $username won: $$message[1];1];sub;<@$authorID> you lost: $$message[1];1]]
+$footer[$username#$discriminator[$authorID]]
+$footerIcon[$userAvatar[$authorID]]
+$color[$replaceText[$replaceText[$randomText[sum;sub];sum;1bbe00;1];sub;eb0000;1]]
+
+$onlyIf[$checkContains[$toLowercase[$message[2]];red;black]==true;]
+
+$onlyIf[$message[1]<=$getUserVar[money];]
+
+$argsCheck[>1;]`
+})
+ 
